cbca31024f3061c9d83df3e1016d1e90a3315597
AndrewJBateman <gomezbateman@yahoo.com>
2022-12-06 12:39:11 +0100

    styles: remove unused CSS, improve card styling

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Observable } from 'rxjs';
-import { Book } from 'src/app/models/google-api.interface';
+import { Observable } from "rxjs";
+import { Book } from "src/app/models/google-api.interface";
 import { GoogleBookService } from "src/app/services/google-book.service";
 
 @Component({
@@ -14,11 +14,10 @@ export class BookListComponent implements OnInit {
   constructor(private googleBookApiService: GoogleBookService) {}
 
   ngOnInit(): void {
-    this.initialBookQuery("google history");
+    this.initialBookQuery("angular tutorial");
   }
 
   initialBookQuery(bookTitle: string) {
     this.books = this.googleBookApiService.findBook(bookTitle);
-    console.log("initial books", this.books);
   }
 }
